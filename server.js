afa7ebf1537448a2f18a6693fce9d7a938443673
Hanlly da Silva <hanlly.silvae@gmail.com>
2024-08-19 10:05:41 -0300

    Update server.js locally

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 require('dotenv').config({path: `${__dirname}/config.env`})
 
 const port = process.env.PORT || 80
-
+#add a comment in git repo (local)
 app.listen(port, err => {
     if(err){
         console.log('Something went wrong with the server...')
